b856d01e3e55b0b31b3b786ae4e0a0e8b3f1831c
Andrew <abadia962@gmail.com>
2022-07-20 23:31:47 -0500

    Aplicacion de operaciones matematicas.

---
+++ mateApp/app/src/main/java/com/afam/mateapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.afam.mateapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
